44e2dc60c748635e27b536d670e60659e42a8bdf
Orcryx <lauryannmagar@gmail.com>
2024-06-02 10:35:21 +0200

    Reprendre à zero la page index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,3 @@
 <?php
 
-use src\service;
-require "../vendor/autoload.php"; // c'est le seul require autorisé !!! 
-
-
-$router = new Router();
-
-$router->get('/public/index', ['controller\home','getHome']); 
-$router->get('/public/blog', ['controller\blog' , 'getGallery']);
-$router->get('/public/contact', ['controller\contact' , 'getForm']); 
-// $router->get('/public/blog', ['controller\blog', 'getPost']);
-//$router->get('/public/blog', ['controller\blog', 'getPost'], $id);
-
-//afficher le retour du routeur
-echo $router->run($_SERVER['REQUEST_URI']);
+echo "je suis la page index.php";
